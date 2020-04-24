b606992482c2c037d039329b3d6e95ff139f5b85
Christoph B <christoph.beger@medizin.uni-leipzig.de>
2020-04-24 12:36:12 +0200

    Replace BETWEEN with one-sided closed interval

--- gold_standard_query.sql
+++ gold_standard_query.sql
@@ -5,7 +5,8 @@ WHERE
   gender = 'M'
 
   -- inclusion: age between 40 and 65
-  AND extract(year FROM age(birthdate)) BETWEEN 40 AND 65
+  AND extract(year FROM age(birthdate)) >= 40
+  AND extract(year FROM age(birthdate)) < 66
 
   -- inclusion: at least one observation of systolic blood pressure >= 130 mmHg in the last year
   AND EXISTS (
