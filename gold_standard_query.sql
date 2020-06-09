ac5136e04d334640d5518fd9efefadc052bc23c9
Christoph B <christoph.beger@medizin.uni-leipzig.de>
2020-06-09 10:53:22 +0200

    Update sql with more performant version

--- gold_standard_query.sql
+++ gold_standard_query.sql
@@ -1,26 +1,24 @@
-SELECT DISTINCT id
-FROM patients p
-WHERE
-  -- inclusion: male subjects
-  gender = 'M'
+SELECT count(DISTINCT id)
+FROM (
+		SELECT p.id
+		FROM patients p
+			JOIN observations ON (patient = p.id)
+		WHERE
+			-- inclusion: male subjects
+			gender = 'M'
 
-  -- inclusion: age between 40 and 65
-  AND extract(year FROM age(birthdate)) BETWEEN 40 AND 65
+			-- inclusion: age between 40 and 65
+			AND extract(year FROM age(birthdate)) BETWEEN 40 AND 65
 
-  -- inclusion: at least one observation of systolic blood pressure >= 130 mmHg in the last year
-  AND EXISTS (
-    SELECT 1
-    FROM observations
-      WHERE patient = p.id
-        AND code = '8480-6'
-        AND value::double precision >= 130
-        AND date >= CURRENT_DATE - interval '1 years'
-  )
+			-- inclusion: at least one observation of systolic blood pressure >= 130 mmHg in the last year
+			AND code = '8480-6'
+			AND value::double precision >= 130
+			AND date >= CURRENT_DATE - interval '1 years'
 
-  -- exclusion: myocardial infarction or stroke
-  AND NOT EXISTS (
-    SELECT 1
-    FROM conditions
-      WHERE patient = p.id
-        AND code IN ('22298006', '230690007')
-  );
+		-- exclusion: myocardial infarction or stroke
+		EXCEPT (
+			SELECT patient AS id
+			FROM conditions
+			WHERE code IN ('22298006', '230690007')
+		)
+	) q;
