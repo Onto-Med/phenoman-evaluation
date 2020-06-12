b97782f88763ff975dedfb2ff5fce18f047455c7
Christoph Beger <begerc@medizin.uni-leipzig.de>
2020-06-12 11:05:44 +0000

    Fix for loop syntax

--- scripts/import_fhir.sh
+++ scripts/import_fhir.sh
@@ -2,7 +2,7 @@
 
 cd synthea/output/fhir
 
-for f in *.json;
+for f in *.json; do
   echo @$f;
-  do curl -X POST -sS -H "Content-Type: application/json" -d @$f http://localhost:8080/fhir > /dev/null;
+  curl -X POST -sS -H "Content-Type: application/json" -d @$f http://localhost:8080/fhir > /dev/null;
 done
