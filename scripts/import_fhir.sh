3cac629cc5032e3bb6c661f6b878cc46121e349e
Christoph Beger <begerc@medizin.uni-leipzig.de>
2020-06-12 09:03:40 +0000

    Update url to match docker-compose.yml

--- scripts/import_fhir.sh
+++ scripts/import_fhir.sh
@@ -4,5 +4,5 @@ cd /fhir
 
 for f in *.json;
   echo @$f;
-  do curl -X POST -sS -H "Content-Type: application/json" -d @$f http://127.0.0.1/fhir > /dev/null;
+  do curl -X POST -sS -H "Content-Type: application/json" -d @$f http://localhost:8080/fhir > /dev/null;
 done
