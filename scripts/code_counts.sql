29d0ae11c17f6275188fddec1cd7c581bd234c9f
Christoph Beger <begerc@medizin.uni-leipzig.de>
2020-06-12 09:00:50 +0000

    Add sorting to code count sql

--- scripts/code_counts.sql
+++ scripts/code_counts.sql
@@ -1,3 +1,3 @@
-SELECT code, count(*) FROM observations GROUP BY code;
-SELECT code, count(*) FROM conditions GROUP BY code;
-SELECT code, count(*) FROM allergies GROUP BY code;
+SELECT code, count(*) FROM observations GROUP BY code ORDER BY 2;
+SELECT code, count(*) FROM conditions GROUP BY code ORDER BY 2;
+SELECT code, count(*) FROM allergies GROUP BY code ORDER BY 2;
