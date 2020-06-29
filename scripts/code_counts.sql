5caec55093211b598f6d5dd8e83034fee96fb692
Christoph Beger <begerc@medizin.uni-leipzig.de>
2020-06-29 07:11:57 +0000

    Remove count codes script

--- scripts/code_counts.sql
+++
@@ -1,3 +0,0 @@
-SELECT code, count(*) FROM observations GROUP BY code ORDER BY 2;
-SELECT code, count(*) FROM conditions GROUP BY code ORDER BY 2;
-SELECT code, count(*) FROM allergies GROUP BY code ORDER BY 2;
